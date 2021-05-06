9091a5f9b0beb5f94f2897dfca74ee6970982c7f
André Ribas <andre.ribas@ebanx.com>
2021-05-06 20:26:01 -0300

    Small models adjustments

--- JedaiTraining/src/models/Cliente.java
+++ JedaiTraining/src/models/Cliente.java
@@ -13,47 +13,54 @@ public class Cliente {
 	private String email;
 	private String sexo;
 	private Date criadoEm;
-	
-	public void setNome(String nome) {	
+
+	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getNome() {
 		return this.nome;
-	}	
+	}
+
 	public String getMatricula() {
 		return matricula;
 	}
+
 	public void setMatricula(String matricula) {
 		this.matricula = matricula;
-	}	
-	
+	}
+
 	public double getIdade() {
 		return idade;
 	}
+
 	public void setIdade(double idade) {
 		this.idade = idade;
-	}	
-	
+	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
-	}	
-	
+	}
+
 	public String getSexo() {
 		return sexo;
 	}
+
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
-	}	
-	
+	}
+
 	public Date getCriadoEm() {
 		return criadoEm;
 	}
+
 	public void setCriadoEm(Date criadoEm) {
 		this.criadoEm = criadoEm;
 	}
-	
+
 }
 
--- JedaiTraining/src/models/Treino.java
+++ JedaiTraining/src/models/Treino.java
@@ -3,21 +3,24 @@ package models;
 import java.util.Date;
 
 public class Treino {
-	
+
 	private String treino;
 	private Date dia;
-	
+
 	public String getTreino() {
 		return treino;
 	}
+
 	public void setTreino(String treino) {
 		this.treino = treino;
 	}
+
 	public Date getDia() {
 		return dia;
 	}
+
 	public void setDia(Date dia) {
 		this.dia = dia;
 	}
-	
+
 }
